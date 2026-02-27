f0777ce1ab4ec337d965f82c4305a443fb20826a
abenezer@csa <abenezerhaymanot@gmail.com>
2026-02-27 12:15:58 +0300

    new lesson on pressable, touchableOpacity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,6 @@
 
-import { Alert, StyleSheet, Text, Image, Button } from "react-native";
+import { Alert, StyleSheet, Text, Image, Button, TouchableOpacity, Pressable} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-
-
 
 export default function RootLayout() {
   const  onButtonPress = () => Alert.alert(
@@ -20,24 +18,27 @@ export default function RootLayout() {
         <SafeAreaView style={styles.container}>
           <Text style={styles.name}> Temar Lije</Text>
           <Button title="Press Me" onPress={onButtonPress} />
-          <Image width={2000} height={100} source={require("../assets/images/react-logo.png")} />
-          <Text style={styles.subtitle}>
 
+          
+          <Pressable onPress={onButtonPress}>
+          <Image
+            width={2000}
+            height={100}
+            blurRadius={15}
+            // style={styles.pic_one}
+            source={require("../assets/images/react-logo.png")
+          } />
+          </Pressable>
+          <Text style={styles.subtitle}>
              Pagination 
              is commonly used in applications that display data in a paginated format, such as search results, product listings, or user records. It helps improve performance and user experience by loading and displaying only a portion of the data at a time, reducing the amount of data transferred and processed.
-              Pagination affects every element of an application's performance, from database queries to network load to user experience. However, poorly optimized pagination can also affect all aspects of an application and can lead to slow response times, increased resource utilization, and poor scalability.
-
-
-             <Text style={{
-               color: "red",
-               textDecorationLine: "underline"
-               
-             }}
-            onPress={()=> Alert.alert("Text Pressed")}
-              >Pagination </Text>Thus, pagination is a critical performance optimization in Postgres when building high-performance applications because it directly affects their efficiency, scalability, and user experience.
-
-To address these challenges, Postgres offers various pagination techniques and optimizations. Let’s review these pagination techniques to see their advantages and limitations </Text>
-          <Image width={2000} height={100} source={require("../assets/images/react-logo.png")} />
+              Pagination affects every element of an application's performance, from database queries to network load to user experience. However, poorly optimized pagination can also affect all aspects of an application and can lead to slow response times, increased resource utilization, and poor scalability.</Text>
+            <TouchableOpacity>
+          <Image
+             width={2000}
+             height={100}
+             source={require("../assets/images/react-logo.png")} />
+          </TouchableOpacity>
         </SafeAreaView>
       </SafeAreaProvider>
   );
@@ -58,6 +59,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "justify",
     color: '#fff',
-  }
+  } 
   
 })
